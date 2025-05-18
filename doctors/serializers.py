11d698cdaad06d4dfae9bc76438b78d9a6ff95af
C0ncatS <ahmedselo2005@gmail.com>
2025-05-18 22:38:45 +0300

    Add doctor serializers and views for login, creation, and update
    - Created ClinicSerializer, DoctorLoginSerializer, DoctorCreateSerializer, and DoctorUpdateSerializer
    - Added DoctorLoginView, DoctorCreateView, and DoctorRetrieveUpdateView

---
+++ doctors/serializers.py
@@ -0,0 +1,170 @@
+from django.utils.translation import gettext as _
+from django.contrib.auth import authenticate
+
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from users.models import CustomUser as User
+from users.serializers import UserUpdateDestroySerializer
+from .models import Doctor, Specialty, Clinic
+
+
+class ClinicSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Clinic
+        fields = [
+            "location",
+            "longitude",
+            "latitude",
+            "phone",
+        ]
+        extra_kwargs = {
+            # Remove DRF's automatic UniqueValidator on `phone`
+            "phone": {"validators": []}
+        }
+
+    def validate_phone(self, value):
+        queryset = Clinic.objects.all()
+        validator = UniqueValidator(queryset=queryset)
+        serializer_field = self.fields["phone"]
+        validator(value, serializer_field)
+        return value
+
+    def create(self, validated_data):
+        doctor = self.context.get("doctor")
+        return Clinic.objects.create(doctor=doctor, **validated_data)
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+
+class DoctorLoginSerializer(serializers.Serializer):
+    phone = serializers.CharField()
+    password = serializers.CharField(write_only=True, style={"input_type": "password"})
+
+    def validate(self, data):
+        phone = data.get("phone")
+        password = data.get("password")
+        user = authenticate(phone=phone, password=password)
+        if (
+            user is None
+            or user.role != User.Role.DOCTOR.value
+            or user.deleted_at is not None
+        ):
+            raise serializers.ValidationError(_("Invalid credentials!"))
+        if not user.is_verified_phone:
+            raise serializers.ValidationError(
+                _("Please, verify your phone number first")
+            )
+        refresh_token = RefreshToken.for_user(user)
+        access_token = refresh_token.access_token
+        expires_in = int(access_token.lifetime.total_seconds())
+        return {
+            "access_token": str(access_token),
+            "refresh_token": str(refresh_token),
+            "expires_in": expires_in,
+        }
+
+
+class DoctorCreateSerializer(serializers.ModelSerializer):
+    specialties = serializers.SlugRelatedField(
+        slug_field="name",
+        queryset=Specialty.objects.all(),
+        many=True,
+    )
+    clinic = ClinicSerializer()
+
+    class Meta:
+        model = Doctor
+        fields = [
+            "about",
+            "education",
+            "start_work_date",
+            "certificate",
+            "status",
+            "specialties",
+            "clinic",
+        ]
+        read_only_fields = ["status"]
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        clinic_data = validated_data.pop("clinic")
+        specialties = validated_data.pop("specialties")
+
+        doctor = Doctor.objects.create(user=user, **validated_data)
+        doctor.specialties.set(specialties)
+
+        clinic_serializer = ClinicSerializer(
+            data=clinic_data, context={"doctor": doctor}
+        )
+        clinic_serializer.is_valid(raise_exception=True)
+        clinic_serializer.save()
+
+        return doctor
+
+
+class DoctorUpdateSerializer(serializers.ModelSerializer):
+    user = UserUpdateDestroySerializer()
+    specialties = serializers.SlugRelatedField(
+        many=True,
+        slug_field="name",
+        queryset=Specialty.objects.all(),
+    )
+    clinic = ClinicSerializer()
+
+    class Meta:
+        model = Doctor
+        fields = [
+            "user",
+            "about",
+            "education",
+            "start_work_date",
+            "status",
+            "specialties",
+            "clinic",
+        ]
+        read_only_fields = ["status"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance and hasattr(self.instance, "clinic"):
+            self.fields["clinic"].instance = self.instance.clinic
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop("user")
+        specialties_data = validated_data.pop("specialties")
+        clinic_data = validated_data.pop("clinic")
+
+        user = instance.user
+        user_serializer = UserUpdateDestroySerializer(
+            instance=user,
+            data=user_data,
+            partial=True,
+        )
+        user_serializer.is_valid(raise_exception=True)
+        user_serializer.save()
+
+        instance.about = validated_data.get("about")
+        instance.education = validated_data.get("education")
+        instance.start_work_date = validated_data.get("start_work_date")
+        instance.save()
+
+        instance.specialties.set(specialties_data)
+
+        clinic = instance.clinic
+        clinic_serializer = ClinicSerializer(
+            instance=clinic,
+            data=clinic_data,
+            partial=True,
+        )
+        clinic_serializer.is_valid(raise_exception=True)
+        clinic_serializer.save()
+
+        return instance
--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import ScopedRateThrottle
 
-# Create your views here.
+from .models import Doctor
+from users.models import CustomUser as User
+from users.permissions import HasRole
+from .serializers import (
+    DoctorLoginSerializer,
+    DoctorCreateSerializer,
+    DoctorUpdateSerializer,
+)
+
+
+class DoctorLoginView(generics.GenericAPIView):
+    serializer_class = DoctorLoginSerializer
+    throttle_classes = [ScopedRateThrottle]
+    throttle_scope = "login"
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+
+
+class DoctorCreateView(generics.CreateAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class DoctorRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Doctor.objects.filter(user__deleted_at__isnull=True)
+    serializer_class = DoctorUpdateSerializer
+    permission_classes = [IsAuthenticated, HasRole]
+    required_roles = [User.Role.DOCTOR]
