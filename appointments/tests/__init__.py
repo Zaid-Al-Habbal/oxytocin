b83d560759673d7a26eb0467183be6c635750068
Zaid Al Habbal <alhabbalzaid10@gmail.com>
2025-07-17 12:56:58 +0300

    test change appointment status

--- appointments/tests/__init__.py
+++ appointments/tests/__init__.py
@@ -2,3 +2,4 @@ from .test_list_clinic_visit_times import *
 from .test_book_appointment import *
 from .test_cancel_appointment import *
 from .test_update_appointment import *
+from .test_change_appointment_status import *
