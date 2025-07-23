9a42243c459f23bb3cab602cc16c4bc207f70259
C0ncatS <ahmedselo2005@gmail.com>
2025-07-23 23:18:32 +0300

    Fix missing model field in ClinicPatientSerializer.Meta class

--- patients/serializers/clinic.py
+++ patients/serializers/clinic.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from clinics.models import ClinicPatient
 from clinics.serializers.summary import ClinicSummarySerializer
 
 
@@ -7,5 +8,6 @@ class ClinicPatientSerializer(serializers.ModelSerializer):
     clinic = ClinicSummarySerializer(read_only=True)
 
     class Meta:
+        model = ClinicPatient
         fields = ["id", "clinic", "cost", "created_at", "updated_at"]
         read_only_fields = ["id", "cost", "created_at", "updated_at"]
