3f04fb803f5e7c0d2e03d18530470e39258bbf10
C0ncatS <ahmedselo2005@gmail.com>
2025-08-29 12:20:44 +0300

    Refactor admin filters for BannedPatient and Evaluation models to include patient and clinic selection. Update Arabic translations for 'Clinic' and 'Patient'. Comment out seeder service in compose files.

--- clinics/admin.py
+++ clinics/admin.py
@@ -1,3 +1,4 @@
+from django.utils.translation import pgettext_lazy
 from django.contrib import admin
 from django.contrib.gis.db import models
 
@@ -7,6 +8,8 @@ from import_export.admin import ImportExportModelAdmin
 from unfold.contrib.import_export.forms import ExportForm, ImportForm, SelectableFieldsExportForm
 
 import mapwidgets
+
+from patients.models import Patient
 
 from .models import BannedPatient, Clinic, ClinicImage
 
@@ -29,11 +32,37 @@ class ClinicAdmin(SimpleHistoryAdmin, ModelAdmin, ImportExportModelAdmin):
     }
 
 
+class BannedPatientClinicFilter(admin.SimpleListFilter):
+    title = pgettext_lazy("the_clinic", "Clinic")
+    parameter_name = "clinic_id"
+    
+    def lookups(self, request, model_admin):
+        return [(clinic.id, str(clinic)) for clinic in Clinic.objects.all()]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(clinic_id=self.value())
+        return queryset
+
+class BannedPatientPatientFilter(admin.SimpleListFilter):
+    title = pgettext_lazy("the_patient", "Patient")
+    parameter_name = "patient_id"
+    
+    def lookups(self, request, model_admin):
+        return [(patient.id, str(patient)) for patient in Patient.objects.all()]
+        
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(patient_id=self.value())
+        return queryset
+    
+
 @admin.register(BannedPatient)
 class BannedPatientAdmin(SimpleHistoryAdmin, ModelAdmin, ImportExportModelAdmin):
     import_form_class = ImportForm
     export_form_class = SelectableFieldsExportForm
     
     list_display = ["id", "clinic", "patient", "created_at"]
+    list_filter = [BannedPatientClinicFilter, BannedPatientPatientFilter]
     readonly_fields = ["created_at"]
     search_fields = ["clinic__phone", "patient__first_name", "patient__last_name"]
--- evaluations/admin.py
+++ evaluations/admin.py
@@ -1,9 +1,13 @@
+from django.utils.translation import pgettext_lazy
+
 from django.contrib import admin
 from unfold.admin import ModelAdmin
-from unfold.contrib.filters.admin import SliderNumericFilter
+from unfold.contrib.filters.admin import SliderNumericFilter, RangeDateFilter
 from import_export.admin import ImportExportModelAdmin
 from unfold.contrib.import_export.forms import ExportForm, ImportForm, SelectableFieldsExportForm
 from simple_history.admin import SimpleHistoryAdmin
+
+from patients.models import Patient
 
 from .models import Evaluation
 
@@ -12,6 +16,17 @@ class RateFilter(SliderNumericFilter):
     MAX_DECIMALS = 5
     STEP = 1
 
+class EvaluationPatientFilter(admin.SimpleListFilter):
+    title = pgettext_lazy("the_patient", "Patient")
+    parameter_name = "patient_id"
+    
+    def lookups(self, request, model_admin):
+        return [(patient.id, str(patient)) for patient in Patient.objects.all()]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(patient_id=self.value())
+        return queryset
 
 @admin.register(Evaluation)
 class EvaluationAdmin(SimpleHistoryAdmin, ModelAdmin, ImportExportModelAdmin):
@@ -19,11 +34,12 @@ class EvaluationAdmin(SimpleHistoryAdmin, ModelAdmin, ImportExportModelAdmin):
     export_form_class = SelectableFieldsExportForm
     
     list_display = ["id", "patient", "appointment", "rate", "created_at", "updated_at"]
+    list_filter_submit = True
     list_filter = [
-        ("patient", admin.RelatedOnlyFieldListFilter),
+        EvaluationPatientFilter,
         ("rate", RateFilter),
-        "created_at",
-        "updated_at",
+        ("created_at", RangeDateFilter),
+        ("updated_at", RangeDateFilter),
     ]
-    list_editable = ["rate"]
+    search_fields = ["patient__user__first_name", "patient__user__last_name", "patient__user__phone"]
     readonly_fields = ["created_at", "updated_at"]
